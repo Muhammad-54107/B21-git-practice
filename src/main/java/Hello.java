ef95d908c0ad9024a7e322ee1feaa8d8c58f8e8b
Muhammad-54107 <Muhammad.qureshi107@gmail.com>
2021-01-26 15:16:23 -0500

    Movie 1 branch updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,8 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("adding new files on git");
+        System.out.println("adding new files on git, B21");
+        System.out.println("B21 future SDET learning Git ");
+
     }
 }
